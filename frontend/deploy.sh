f0a548138fd6a243e313c847402626563209c391
Stefan Kruik <s.kruik@icloud.com>
2025-02-05 22:36:56 +0100

    CI/CD FIx

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export HOME=/home/svkruik
-export PATH=/root/.nvm/versions/node/v20.15.1/bin:$PATH
 
 # Git
 cd ..
-git config --global --add safe.directory /home/svkruik/Documents/GitHub/SK-Metrics
+git config --global --add safe.directory "$HOME/Documents/GitHub/SK-Metrics"
 git reset --hard
 git pull
 echo "Git setup complete"
